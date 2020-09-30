a7812ba6755df096d4dfec4239e0c3c8fb34b1c1
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-09-30 17:23:02 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("req at ", req.url);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
